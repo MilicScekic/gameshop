b2e353cf0591e3b60ed3fce3c053319d7a867a30
Sinisa <sinisa.becic@outlook.com>
2022-07-19 03:35:44 +0200

    Redux added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
