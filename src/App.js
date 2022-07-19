1dbeb6162e16871feafed91d9b45da9c168e0ebe
Sinisa <sinisa.becic@outlook.com>
2022-07-19 03:37:37 +0200

    Changed to Router v5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import Layout from "./layouts/Layout";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import About from "./pages/About";
@@ -11,8 +11,11 @@ import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { pink, purple } from "@mui/material/colors";
 import { AuthContextProvider } from "./contexts/AuthContext";
 import Product from "./pages/Product";
-import { PrivateRoute } from "./hoc/PrivateRoute";
+import PrivateRoute from "./hoc/PrivateRoute";
 import Cart from "./pages/Cart";
+import Products from "./pages/Products";
+import { LastLocationProvider } from "react-router-last-location";
+import { connect } from "react-redux";
 import { CartContextProvider } from "./contexts/CartContext";
 
 const theme = createTheme({
@@ -31,31 +34,32 @@ function App() {
     <ThemeProvider theme={theme}>
       <CartContextProvider>
         <AuthContextProvider>
-          <BrowserRouter>
-            <Layout>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
+          <Router>
+            <LastLocationProvider>
+              <Layout>
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/shop" component={Shop} />
+                  <Route exact path="/cart" component={Cart} />
+                  <Route exact path="/about" component={About} />
+                  <Route path="/contact" component={Contact} />
 
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/register" component={Register} />
 
-                {/* <Route
-                path="/admin"
-                element={<PrivateRoute children={<Dashboard />} />}
-              /> */}
+                  {/* <PrivateRoute exact path="/admin" component={Dashboard} /> */}
 
-                <Route path="/product/:name" element={<Product />} />
-              </Routes>
-            </Layout>
-          </BrowserRouter>
+                  <Route exact path="/products" component={Products} />
+                  <Route exact path="/product/:name" component={Product} />
+                </Switch>
+              </Layout>
+            </LastLocationProvider>
+          </Router>
         </AuthContextProvider>
       </CartContextProvider>
     </ThemeProvider>
   );
 }
 
-export default App;
+// export default App;
+export default connect(null)(App);
