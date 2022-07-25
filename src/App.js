54841da0df05833ffe57f272dc249f3388f0a75c
Sinisa <sinisa.becic@outlook.com>
2022-07-25 07:37:37 +0200

    Dodata funkcionalnost za sesiju i prijavu pri ponovnom ucitavanju stranice

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { useContext, useEffect } from "react";
 import Layout from "./layouts/Layout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Shop from "./pages/Shop";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { pink, purple } from "@mui/material/colors";
-import { AuthContextProvider } from "./contexts/AuthContext";
+import { AuthContextProvider } from "./contexts/AuthContext"; //? vise se nece koristiti
 import Product from "./pages/Product";
 import PrivateRoute from "./hoc/PrivateRoute";
 import Cart from "./pages/Cart";
@@ -17,7 +16,7 @@ import Products from "./pages/Products";
 import { LastLocationProvider } from "react-router-last-location";
 import { connect } from "react-redux";
 import Favorites from "./pages/Favorites";
-import { autoSigninUser } from "./store/actions/auth";
+import { autoSigninUser, logoutAfterSession } from "./store/actions/auth";
 // import { CartContextProvider } from "./contexts/CartContext";
 
 const theme = createTheme({
@@ -31,10 +30,16 @@ const theme = createTheme({
   },
 });
 
-function App({ autoSigninUser }) {
-  // useEffect(() => {
-  //   if (localStorage.token) autoSigninUser();
-  // }, []);
+function App({ autoSigninUser, logoutAfterSession }) {
+  //? Moze posluziti...
+  // const [isLogged, setIsLogged] = useState(!!localStorage.getItem('token'));
+
+  //* Ukoliko ima token u local storage, prijavi tog korisnika
+  useEffect(() => {
+    localStorage.token
+      ? autoSigninUser(localStorage.token) && logoutAfterSession(60) // U minutima
+      : delete localStorage.token;
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -45,9 +50,7 @@ function App({ autoSigninUser }) {
             <Layout>
               <Switch>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/shop" component={Shop} />
                 <Route exact path="/cart" component={Cart} />
-                <Route exact path="/favorites" component={Favorites} />
                 <Route exact path="/about" component={About} />
                 <Route path="/contact" component={Contact} />
 
@@ -55,9 +58,11 @@ function App({ autoSigninUser }) {
                 <Route exact path="/register" component={Register} />
 
                 {/* <PrivateRoute exact path="/admin" component={Dashboard} /> */}
+                <PrivateRoute exact path="/favorites" component={Favorites} />
+                {/* <Route exact path="/favorites" component={Favorites} /> */}
 
                 <Route exact path="/products" component={Products} />
-                <Route exact path="/product/:name" component={Product} />
+                <Route exact path="/product/:id" component={Product} />
               </Switch>
             </Layout>
           </LastLocationProvider>
@@ -68,4 +73,4 @@ function App({ autoSigninUser }) {
   );
 }
 
-export default connect(null, { autoSigninUser })(App);
+export default connect(null, { autoSigninUser, logoutAfterSession })(App);
