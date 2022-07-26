813a2470d79358a5414adb0d496101720f0c20be
Sinisa <sinisa.becic@outlook.com>
2022-07-26 05:18:34 +0200

    Problem sa Corsom rijesen

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -16,38 +16,28 @@ const Products = ({
   const [page, setPage] = useState(1);
   const lastLocation = useLastLocation();
 
-  const currentURL = `${location.pathname}${location.search}`;
-  const prevLocation = useLastLocation();
-  let prevURL;
-  if (prevLocation)
-    prevURL = `${prevLocation.pathname}${prevLocation.search}` || null;
+  // const currentURL = `${location.pathname}${location.search}`;
+  // const prevLocation = useLastLocation();
+  // let prevURL;
+  // if (prevLocation)
+  //   prevURL = `${prevLocation.pathname}${prevLocation.search}` || null;
 
   useEffect(() => {
     // in case user was searching desktops and clicked 'products' in header again
-    // if (lastLocation && lastLocation.pathname === '/products') clearProducts()
+    if (lastLocation && lastLocation.pathname === "/products") clearProducts();
 
     showSpinner();
 
-    //! Ukoliko ovo ostavimo, onda pri kliku na neku od kategorija nece prikazati proizvode za trazenu kategoriju
-    // if (lastLocation && location.pathname !== lastLocation.pathname) {
-    //   clearProducts();
-    //   history.replace("/products"); // go back to default settings with no filters
-    // }
+    const targetURL = `https://gameshop-g5.com/products/?format=json`;
 
-    const targetURL = `http://localhost:4443/api/products${
-      location.search ? location.search : "?"
-    }${location.search && "&"}page=${page}`;
     const timeoutId = setTimeout(() => {
-      // this timeout fixes the problem with mixed products over time
-      // I don't know why that was caused, but sometimes we had e.g.
-      // mixed smartphones and laptops etc.
       getProducts(targetURL);
     }, 200);
 
     return () => {
       clearTimeout(timeoutId);
     };
-  }, [location.search, page]);
+  }, []);
 
   return (
     <Fragment>
