fccb84236ce3ecd1451f0c67d048f91c40000ddd
Sinisa <sinisa.becic@outlook.com>
2022-08-16 04:46:41 +0200

    Komponente za pretragu prenesene u style

--- src/components/Search/SearchBar.js
+++ src/components/Search/SearchBar.js
@@ -1,128 +1,26 @@
 import { useState, useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 import { IoSearch, IoClose } from "react-icons/io5";
 import { motion, AnimatePresence } from "framer-motion";
 import { useClickOutside } from "react-click-outside-hook";
 import MoonLoader from "react-spinners/MoonLoader";
 import useDebounce from "../../hooks/debounceHook";
 import ProductItem from "./product";
-import axios from "axios";
-
-const SearchBarContainer = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  width: 34em;
-  height: 3.8em;
-  background-color: #fff;
-  border-radius: 6px;
-  box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.14);
-  overflow: hidden;
-`;
-
-const SearchInputContainer = styled.div`
-  width: 100%;
-  min-height: 4em;
-  display: flex;
-  align-items: center;
-  position: relative;
-  padding: 2px 15px;
-`;
-
-const SearchInput = styled.input`
-  width: 100%;
-  height: 100%;
-  outline: none;
-  border: none;
-  font-size: 21px;
-  color: #12112e;
-  font-weight: 500;
-  border-radius: 6px;
-  background-color: transparent;
-
-  &:focus {
-    outline: none;
-    &::placeholder {
-      opacity: 0;
-    }
-  }
-  &::placeholder {
-    color: #bebebe;
-    transition: all 250ms ease-in-out;
-  }
-`;
-
-const SearchIcon = styled.span`
-  color: #bebebe;
-  font-size: 27px;
-  margin-right: 10px;
-  margin-top: 6px;
-  vertical-align: middle;
-`;
-
-const CloseIcon = styled(motion.span)`
-  color: #bebebe;
-  font-size: 23px;
-  vertical-align: middle;
-  transition: all 200ms ease-in-out;
-  cursor: pointer;
-
-  &:hover {
-    color: #dfdfdf;
-  }
-`;
-
-const LineSeparator = styled.span`
-  display: flex;
-  min-width: 100%;
-  min-height: 2px;
-  background-color: #d8d8d878;
-`;
-
-const CountResult = styled.span`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 100%;
-  min-height: 1rem;
-  color: #000;
-  // background-color: #d8d8d878;
-`;
-
-const SearchContent = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  padding: 1em;
-  overflow-y: auto;
-`;
-
-const LoadingWrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const WarningMessage = styled.span`
-  color: #a1a1a1;
-  font-size: 14px;
-  display: flex;
-  align-self: center;
-  justify-self: center;
-`;
-
-const containerVariants = {
-  expanded: {
-    height: "20em",
-  },
-  collapsed: {
-    height: "3.8em",
-  },
-};
+// import axios from "axios";
+import {
+  SearchBarContainer,
+  SearchInputContainer,
+  SearchInput,
+  SearchIcon,
+  CloseIcon,
+  LineSeparator,
+  CountResult,
+  SearchContent,
+  LoadingWrapper,
+  WarningMessage,
+  containerVariants,
+} from "./style";
 
 const containerTransition = { type: "spring", damping: 22, stiffness: 150 };
 
--- src/components/Search/style.js
+++ src/components/Search/style.js
@@ -1,45 +1,116 @@
-import { styled, alpha } from "@mui/material/styles";
-import InputBase from "@mui/material/InputBase";
+import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 
-export const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: "0.75rem !important",
-  //   backgroundColor: "#31373E !important",
-  backgroundColor: "#000 !important",
-  "&:hover": {
-    backgroundColor: "#000 !important",
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
+export const SearchBarContainer = styled(motion.div)`
+  display: flex;
+  flex-direction: column;
+  width: 45em;
+  height: 3.8em;
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0px 2px 12px 3px rgba(0, 0, 0, 0.14);
+  overflow: hidden;
+`;
 
-export const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+export const SearchInputContainer = styled.div`
+  width: 100%;
+  min-height: 4em;
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding: 2px 15px;
+`;
 
-export const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "30ch",
-      "&:focus": {
-        width: "50ch",
-      },
-    },
+export const SearchInput = styled.input`
+  width: 100%;
+  height: 100%;
+  outline: none;
+  border: none;
+  font-size: 21px;
+  color: #12112e;
+  font-weight: 500;
+  border-radius: 6px;
+  background-color: transparent;
+
+  &:focus {
+    outline: none;
+    &::placeholder {
+      opacity: 0;
+    }
+  }
+  &::placeholder {
+    color: #bebebe;
+    transition: all 250ms ease-in-out;
+  }
+`;
+
+export const SearchIcon = styled.span`
+  color: #bebebe;
+  font-size: 27px;
+  margin-right: 10px;
+  margin-top: 6px;
+  vertical-align: middle;
+`;
+
+export const CloseIcon = styled(motion.span)`
+  color: #bebebe;
+  font-size: 23px;
+  vertical-align: middle;
+  transition: all 200ms ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    color: #dfdfdf;
+  }
+`;
+
+export const LineSeparator = styled.span`
+  display: flex;
+  min-width: 100%;
+  min-height: 2px;
+  background-color: #d8d8d878;
+`;
+
+export const CountResult = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 100%;
+  min-height: 1rem;
+  color: #000;
+  // background-color: #d8d8d878;
+`;
+
+export const SearchContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding: 1em;
+  overflow-y: auto;
+`;
+
+export const LoadingWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const WarningMessage = styled.span`
+  color: #a1a1a1;
+  font-size: 14px;
+  display: flex;
+  align-self: center;
+  justify-self: center;
+`;
+
+export const containerVariants = {
+  expanded: {
+    height: "20em",
   },
-}));
+  collapsed: {
+    height: "3.8em",
+  },
+};
